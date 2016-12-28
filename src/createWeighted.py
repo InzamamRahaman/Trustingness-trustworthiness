676c75365326f9a842e31fb2de3c03efd35984ff
InzamamRahaman <izzyrahaman@gmail.com>
2016-12-28 15:35:05 -0400

    Fixed adding the weights

--- src/createWeighted.py
+++ src/createWeighted.py
@@ -7,13 +7,18 @@ parser.add_argument('output', help='The path to the output file')
 args = parser.parse_args()
 
 graph = nx.read_edgelist(args.input, create_using=nx.DiGraph())
+#print(graph[u'3480'])
 for u, neighbours in graph.adjacency_iter():
-    num = float(len(neighbours))
-    w = 1.0 / num
-    for k, _ in neighbours:
-        graph[u][k]['weight'] = w
+    num = len(neighbours)
+    #print(neighbours)
+    #print('node ', u, ' has ', num, ' neighbours')
+    if num > 0:
+        w = 1.0 / float(num)
+        for k, _ in neighbours.iteritems():
+            #print(u, ' , ', k)
+            graph[u][k]['weight'] = w
 
-nx.write_edgelist(graph, args.output)
+nx.write_edgelist(graph, args.output, data=['weight'])
 
 
 
